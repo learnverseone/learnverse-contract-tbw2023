5d70e2857851653744eaad22f7bef4aa571f9286
Daky <ga013077@gmail.com>
2023-12-12 00:40:20 +0800

    use testnet USDT

--- script/MysteryBoxMinter.s.sol
+++ script/MysteryBoxMinter.s.sol
@@ -13,7 +13,9 @@ contract MysteryBoxMinterScript is Script {
         uint256 deployerPrivateKey = vm.envUint("PRIVATE_KEY");
         vm.startBroadcast(deployerPrivateKey);
 
-        address USDT = 0x9e5AAC1Ba1a2e6aEd6b32689DFcF62A509Ca96f3;
+        // address USDT = 0x9e5AAC1Ba1a2e6aEd6b32689DFcF62A509Ca96f3;
+        // use testnet USDT
+        address USDT = 0x7ef95a0FEE0Dd31b22626fA2e10Ee6A223F8a684;
 
         MysteryBoxMinter minter = new MysteryBoxMinter(100 * 1e6, 5000, USDT);
         MysteryBox mysteryBox = minter.mysteryBox();
